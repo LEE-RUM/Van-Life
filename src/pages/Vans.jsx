9fae8f13d0a135ba8395f19f5b21c1431b6c9430
LEE-RUM <lirimehmeti22@gmail.com>
2023-06-26 16:12:48 -0400

    made some changes to vans

--- src/pages/Vans.jsx
+++ src/pages/Vans.jsx
@@ -11,22 +11,24 @@ export default function Vans() {
     }, []);
 
     const vanElements = vans.map((van) => (
-        <div key={van.id} className="bg-white p-4 rounded shadow">
+        <div key={van.id} className="bg-white p-4 rounded-md shadow-md">
             <Link to={`/vans/${van.id}`}>
-            <img src={van.imageUrl} className="w-full h-auto" alt={van.name} />
-            <div className="mt-2">
-                <h3 className="text-xl font-bold text-truncate">{van.name}</h3>
-                <p className="text-gray-700">${van.price}<span className="font-normal">/day</span></p>
-            </div>
-            <i className={`text-sm ${van.type}`}>{van.type}</i>
+                <img src={van.imageUrl} className="w-full" alt={van.name} />
+                <div className="mt-2">
+                    <h3 className="text-xl font-bold text-truncate">{van.name}</h3>
+                    <p className="text-gray-700">${van.price}<span className="font-normal">/day</span></p>
+                </div>
+                <i className={`text-sm ${van.type}`}>{van.type}</i>
             </Link>
         </div>
     ))
 
     return (
-        <section className="container mx-auto pb-8 px-10 lg:px-36">
-            <h1 className="text-center text-2xl font-bold pt-4 pb-4">Explore our van options</h1>
-            <div className="grid grid-cols-2 lg:grid-cols-3 gap-8">{vanElements}</div>
+        <section className="flex flex-col items-center">
+            <div className="sm:min-w-[650px] md:min-w-[700px] max-w-[850px] mt-8 p-3">
+                <h1 className="text-center text-2xl font-bold pt-4 pb-6">Explore our van options</h1>
+                <div className="grid grid-cols-2 lg:grid-cols-3 gap-8 pb-4">{vanElements}</div>
+            </div>
         </section>
     )
 }
