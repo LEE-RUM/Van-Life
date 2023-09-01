b4908443cdda8a4bd48ef1cb8d22cb4439c7bdf7
LEE-RUM <lirimehmeti22@gmail.com>
2023-08-31 23:43:50 -0400

    update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,21 +1,24 @@
 import React from "react";
 import hero from "../assets/hero.jpeg"
+import { Link } from "react-router-dom";
 
-export default function Home(){
-    return(
-        <section
-        className="bg-cover bg-center h-[100vh] flex flex-col items-center justify-center"
-        style={{ backgroundImage: `url(${hero})` }}
-      >
-        <h1 className="text-4xl text-white font-bold mb-6 text-center">
-          You got the travel plans, we got the travel vans.
-        </h1>
-        <p className="text-lg text-white mb-6 text-center">
-          Add adventure to your life by joining the #vanlife movement. Rent the perfect van to make your perfect road trip.
-        </p>
+export default function Home() {
+  return (
+    <section
+      className="bg-cover bg-center h-[100vh] flex flex-col items-center justify-center"
+      style={{ backgroundImage: `url(${hero})` }}
+    >
+      <h1 className="text-4xl text-white font-bold mb-6 text-center">
+        You got the travel plans, we got the travel vans.
+      </h1>
+      <p className="text-lg text-white mb-6 text-center">
+        Add adventure to your life by joining the #vanlife movement. Rent the perfect van to make your perfect road trip.
+      </p>
+      <Link to="/vans">
         <button className="px-10 py-3 bg-orange-400 text-white">
-          Find your vans
+          Find your van
         </button>
-      </section>
-    )
+      </Link>
+    </section>
+  )
 }
